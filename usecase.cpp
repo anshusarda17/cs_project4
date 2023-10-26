a55ba86c1e38d57fc357ce5885e848a2b8f7280d
iamethan12 <105604437+iamethan12@users.noreply.github.com>
2023-10-26 19:47:20 -0400

    What I got for use.case, are we suppose to use the .txt file?

--- usecase.cpp
+++ usecase.cpp
@@ -1,4 +1,53 @@
-#include<iostream>
-
-
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "bst.h" 
 using namespace std;
+
+
+BST<string, string>* create_bst(const string& fname) {
+    auto *bst = new BST<string, string>();
+
+    ifstream file(fname);
+    string line, bin, hex;
+
+    while (getline(file, line)) {
+        stringstream ss(line);
+        getline(ss, bin, ',');
+        getline(ss, hex);
+        bst->insert(hex, bin);
+    }
+
+    return bst;
+}
+
+// Function to convert a binary string to hexadecimal using the BST
+string convert(BST<string, string>* bst, string bin) {
+    // Pad the binary string to make its length a multiple of 4
+    int pad = 4 - (bin.length() % 4);
+    if (pad != 4) {
+        bin = string(pad, '0') + bin;
+    }
+
+    string hex;
+    for (size_t i = 0; i < bin.length(); i += 4) {
+        string segment = bin.substr(i, 4);
+        hex += bst->get(segment);
+    }
+    return hex;
+}
+
+int main() {
+    string filename = "binhex.txt"; 
+    auto *bst = create_bst(filename);
+
+    string binaryInput;
+    cout << "Enter binary representation for conversion: ";
+    cin >> binaryInput;
+
+    string hexOutput = convert(bst, binaryInput);
+    cout << "Hexadecimal representation of " << binaryInput << " is " << hexOutput << endl;
+
+    delete bst;  // Clean up the BST
+    return 0;
+}
